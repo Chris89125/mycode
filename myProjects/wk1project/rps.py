21420bbd9ba7deb11aec9274264d504df5bc50b4
chris89125 <chris.pelletier891@gmail.cm>
2023-04-13 18:55:45 -0400

    wk1projects

---
+++ myProjects/wk1project/rps.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+print("welcome to Rock Paper Scissors")
+username=input("Please enter your name.\n")
+print("Hello ", username,"!",sep='') 
+
+player=input("Please Type R for Rock, P for Paper or S for Scissors\n")
+
+if player=="r" or player=="R":
+   print("You chose Rock!")
+elif player=="p" or player=="P":
+   print("You chose Paper!")
+elif player=="s" or player=="S":
+   print("You chose Scissors!")
+else:
+    print("Not a good choice. Please choose R, P or S.")
+   
+   
