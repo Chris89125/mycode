dfeb547258d922820d75162cdb5523d5f01bf956
Chris89125 <chris.pelletier891@gmail.com>
2023-04-14 18:04:27 +0000

    99bbchall

---
+++ 99bb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+NB=99
+for bottle in range (NB,0,-1): 
+    print(bottle,"bottles of beer on the wall!",bottle,"bottles of beer! Take one down pass it around,", bottle-1,"bottles of beer on the wall!")
