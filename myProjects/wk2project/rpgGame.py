7e5e65f1e89c7dbfe5847b0c8730e126f7226ab8
chris89125 <chris.pelletier891@gmail.cm>
2023-04-18 16:00:13 -0400

    added basic movement

--- myProjects/wk2project/rpgGame.py
+++ myProjects/wk2project/rpgGame.py
@@ -3,6 +3,13 @@
 
 import random
 
+
+VALID_ROOMS = ["GUARD_ROOM", "SCIENCE_LAB", "PUMP_ROOM", "ARMORY" ]
+VALID_CMDS = ["I", "LOOK", "Q"]
+VALID_DIRS = ["D", "U", "E", "W"]
+VALID_ITEMS = ["KEY_CARD", "TERMINAL"]
+
+
 inventory = [] 
 
 def configureRooms():
@@ -26,13 +33,45 @@ def configureRooms():
         }
     return rooms
 
+def show_status(current_room,inventory):
+    print(current_room)
+    print(inventory)
+
 
 def main():
     print("in main")
 
     print("inventory", inventory)
+
     rooms = configureRooms()
     print("rooms", rooms)
+
+    while True:
+
+        move=""
+        while move == "":
+            move=input(". > ")
+
+            move = move.upper().split(" ",1)
+            print( move )
+            if move[0] == "Q": 
+                print("Goodbye Quitter")
+                exit()
+
+
+            if move[0] == "D":
+                print("You Move dowm to the next room")
+            
+            if move[0] == "U":
+                print("You Move up to the next room")
+
+            if move[0] == "W":
+                print("You Move West to the next room")
+
+            if move[0] == "E":
+                print("You Move East to the next room")
+                
+                
 
 if __name__ == "__main__":
     main()
