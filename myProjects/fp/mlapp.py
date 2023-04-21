8684571a9be0778be79ba2e8c1649f5037615be6
chris89125 <chris.pelletier891@gmail.cm>
2023-04-20 22:40:41 -0400

    debugging routes

--- myProjects/fp/mlapp.py
+++ myProjects/fp/mlapp.py
@@ -3,7 +3,9 @@
 """Flask Driver for game website"""
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from flask import redirect
+from flask import url_for 
 
 
 """Flask Constructor"""
@@ -14,13 +16,30 @@ app = Flask(__name__)
 def deafult_route():
     return "Welcome to Mad Libs"
 
-@app.route("/ml_start_pg")
+@app.route("/ml_start_pg", methods=["GET","POST"])
 def ml_start_pg():
-    return render_template("ml_start_pg.html")
+    Verb1="running"
+    Verb2="swim"
+    if request.method == "POST":
+        if request.form.get("V1"):
+            Verb1 = request.form.get("V1")
+            print(Verb1)
 
-@app.route("/ml_results_pg")
-def ml_results_pg():
-    return render_template("ml_result_pg.html")
+        if request.form.get("V2"):
+            Verb2 = request.form.get("V2")
+            print(Verb2)
+
+
+    return redirect(url_for("ml_result_pg", V1 = Verb1, V2 = Verb2))
+    #return redirect(url_for("app.route", V1 = Verb1, V2 = Verb2))
+
+
+
+@app.route("/ml_result_pg/<V1>/<V2>")
+def ml_result_pg(V1,V2):
+   #  return render_template("ml_start_pg",V1,V2)
+   #  return render_template("ml_result_pg.html")
+     return render_template("ml_start_pg.html")
 
 @app.route("/about")
 def about_route():
