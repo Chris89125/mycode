d367357dac8eee796d18e3e84abfd9129dd90af0
chris89125 <chris.pelletier891@gmail.cm>
2023-04-21 17:44:23 -0400

    adding more explations

--- myProjects/fp/mlapp.py
+++ myProjects/fp/mlapp.py
@@ -13,38 +13,42 @@ app = Flask(__name__)
 
 """website routes"""
 @app.route("/")
-def deafult_route():
-    return "Welcome to Mad Libs"
-
-@app.route("/ml_start_pg", methods=["GET","POST"])
-def ml_start_pg():
-    Verb1="running"
-    Verb2="swim"
-    if request.method == "POST":
-        if request.form.get("V1"):
-            Verb1 = request.form.get("V1")
-            print(Verb1)
-
-        if request.form.get("V2"):
-            Verb2 = request.form.get("V2")
-            print(Verb2)
-
-
-    return redirect(url_for("ml_result_pg", V1 = Verb1, V2 = Verb2))
-    #return redirect(url_for("app.route", V1 = Verb1, V2 = Verb2))
-
-
-
-@app.route("/ml_result_pg/<V1>/<V2>")
-def ml_result_pg(V1,V2):
-   #  return render_template("ml_start_pg",V1,V2)
-   #  return render_template("ml_result_pg.html")
+@app.route("/ml_start_pg")
+def ml_result_pg_dsp():
      return render_template("ml_start_pg.html")
 
+"""Render results page. If the user did not enter a variable it will"""
+@app.route("/ml_result_pg/results",methods=["GET","POST"])
+def ml_result_pg():
+    verb1="VERB1"
+    verb2="VERB2"
+    adverb1="ADVERB1"
+    noun1="NOUN1"
+
+    """Capture user input and turns it into a variable
+        If the user did not enter a variable 
+        it will have a deafult value which are listed above"""
+    if request.method == "POST":
+        if request.form.get("v1"):
+            verb1 = request.form.get("v1") 
+
+        if request.form.get("v2"):
+            verb2 = request.form.get("v2")
+        
+        if request.form.get("adv1"):
+            adverb1 = request.form.get("adv1")
+            
+        if request.form.get("n1"):
+            noun1 = request.form.get("n1")
+
+    return render_template("ml_result_pg.html",v1=verb1,v2=verb2,adv1=adverb1,n1=noun1)
+
+
+"""Render the about page"""
 @app.route("/about")
 def about_route():
     return "<h1>Mad Libs Instructions</h1>"
 
 if __name__ == "__main__":
-     #app.run(host="0.0.0.0", port=2224)
-     app.run(host="0.0.0.0", port=2224, debug=True)
+     app.run(host="0.0.0.0", port=2224)
+     #app.run(host="0.0.0.0", port=2224, debug=True)
