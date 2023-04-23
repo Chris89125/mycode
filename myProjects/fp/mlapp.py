fe938748c7a778418dd99de5bee0a025f05a483e
chris89125 <chris.pelletier891@gmail.cm>
2023-04-23 19:51:12 -0400

    Final product for final Project

--- myProjects/fp/mlapp.py
+++ myProjects/fp/mlapp.py
@@ -20,35 +20,85 @@ def ml_result_pg_dsp():
 """Render results page. If the user did not enter a variable it will"""
 @app.route("/ml_result_pg/results",methods=["GET","POST"])
 def ml_result_pg():
-    verb1="VERB1"
-    verb2="VERB2"
-    adverb1="ADVERB1"
+
+    adjective1="ADJ1"
     noun1="NOUN1"
+    noun2="NOUN2"
+    verb1="VERB1"
+    liquid1="LIQUID1"
+    liquid2="LIQUID2"
+    liquid3="LIQUID3"
+    color1="COLOR1"
+    verb2="VERB2"
+    verb3="VERB3"
+    number1="NUMBER1"
+    adjective2="ADJ2"
+    adverb1="ADVERB1"
+    body1="BODY1"
+    verb4="VERB4"
+    verb5="VERB5"
+
 
     """Capture user input and turns it into a variable
         If the user did not enter a variable 
         it will have a deafult value which are listed above"""
     if request.method == "POST":
-        if request.form.get("v1"):
-            verb1 = request.form.get("v1") 
+        if request.form.get("adj1"):
+            adjective1 = request.form.get("adj1") 
 
-        if request.form.get("v2"):
-            verb2 = request.form.get("v2")
-        
-        if request.form.get("adv1"):
-            adverb1 = request.form.get("adv1")
-            
         if request.form.get("n1"):
             noun1 = request.form.get("n1")
 
-    return render_template("ml_result_pg.html",v1=verb1,v2=verb2,adv1=adverb1,n1=noun1)
+        if request.form.get("n2"):
+            noun2 = request.form.get("n2")
+        
+        if request.form.get("v1"):
+            verb1 = request.form.get("v1")
+        
+        if request.form.get("liq1"):
+            liquid1 = request.form.get("liq1")
+
+        if request.form.get("liq2"):
+            liquid2 = request.form.get("liq2")
+
+        if request.form.get("liq3"):
+            liquid3 = request.form.get("liq3")
+
+        if request.form.get("col1"):
+            color1 = request.form.get("col1")
+
+        if request.form.get("v2"):
+            verb2 = request.form.get("v2")       
+
+        if request.form.get("v3"):
+            verb3 = request.form.get("v3")
+
+        if request.form.get("num1"):
+            number1 = request.form.get("num1")
+
+        if request.form.get("adj2"):
+            adjective2 = request.form.get("adj2")
+
+        if request.form.get("adv1"):
+            adverb1 = request.form.get("adv1")
+
+        if request.form.get("bod1"):
+            body1 = request.form.get("bod1")
+
+        if request.form.get("v4"):
+            verb4 = request.form.get("v4")
+
+        if request.form.get("v5"):
+            verb5 = request.form.get("v5")
+
+    return render_template("ml_result_pg.html",adj1=adjective1, n1=noun1, n2=noun2,v1=verb1, liq1=liquid1, liq2=liquid2, liq3=liquid3,col1=color1, v2=verb2, v3=verb3, num1=number1,adj2=adjective2, adv1=adverb1, bod1=body1,v4=verb4, v5=verb5)
 
 
 """Render the about page"""
 @app.route("/about")
 def about_route():
-    return "<h1>Mad Libs Instructions</h1>"
+     return render_template("ml_about_pg.html")
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=2224)
-     #app.run(host="0.0.0.0", port=2224, debug=True)
+     #app.run(host="0.0.0.0", port=2225)
+     app.run(host="0.0.0.0", port=2225, debug=True)
